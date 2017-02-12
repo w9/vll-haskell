32a11ff697d2ef584c96b65500ddc83e7f2c7678
Xun Zhu <zhuxun2@gmail.com>
2017-02-11 18:30:00 -1000

    commit

---
+++ app/VLL.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import System.Process
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let vl = proc "vl" ("-z" : args)
+  (_, Just hout, _, _) <- createProcess vl { std_out = CreatePipe }
+  (_, _, _, p) <- createProcess (proc "less" []){ std_in = UseHandle hout }
+  _ <- waitForProcess p
+  return ()
